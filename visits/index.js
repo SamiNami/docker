e03580a2347ff2a8f6fc490f9609bf35a53f4a55
sami peralahti <sami.peralahti@vainu.io>
2018-08-29 23:41:23 -0700

    docker compose commands

--- visits/index.js
+++ visits/index.js
@@ -10,7 +10,7 @@ client.set("visits", 0);
 
 app.get("/", (req, res) => {
   client.get("visits", (err, visits) => {
-    res.send("Numer of visits", visits);
+    res.send("Number of visits " + visits);
     client.set("visits", parseInt(visits) + 1);
   });
 });
